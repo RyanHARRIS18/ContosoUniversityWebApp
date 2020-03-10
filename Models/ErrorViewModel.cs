93e75a40527357e8bf06f8508f07bf924b3bf901
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2020-03-10 15:53:38 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContosoUniversityWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
